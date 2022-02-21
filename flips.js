b0cc2d8f320c2b47369ac65d4d16d2cc69eac0d9
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 11:01:55 -0500

    modify flips.js to see if something is wrong here

--- flips.js
+++ flips.js
@@ -1,3 +1,4 @@
+/*
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 
 try {
@@ -7,5 +8,20 @@ try {
     console.log(countFlips(coinFlips(number)));
 } 
 catch (error) {
+    console.log(countFlips(coinFlips(1)));
+}
+*/
+
+import { coinFlips, countFlips } from "./modules/coin.mjs";
+
+
+try {
+    let number = 1; 
+    const arg = process.argv.slice(2)[0].slice(9);
+    if( number != null){
+        number = arg;
+        console.log(countFlips(coinFlips(number)));
+    }
+} catch (error) {
     console.log(countFlips(coinFlips(1)));
 }
