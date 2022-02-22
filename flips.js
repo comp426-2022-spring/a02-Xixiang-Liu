413a8ad578fe57439409d6c493e2acba94ec33bf
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 21:13:42 -0500

    erase the extra codes for only one throw in flips.js

--- flips.js
+++ flips.js
@@ -5,13 +5,4 @@ const args = minimist(process.argv.slice(2))
 if (args.number == null) 
     args.number = 1;
 let record = coinFlips(args.number);
-if (args.number <= 1) {
-    let data = countFlips(record)
-    if (data.heads == 1) {
-        console.log("{heads: " + data.heads + "}");
-    } else {
-        console.log("{tails: " + data.tails + "}");
-    }
-} else {
-    console.log(countFlips(record));
-}
+console.log(countFlips(record));
