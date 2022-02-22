22c9ce612ea9de8aabd0a61ae50ae87d1b7f118b
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 21:17:21 -0500

    print record of flips in flips.js as instructed on a02 instruction

--- flips.js
+++ flips.js
@@ -5,4 +5,5 @@ const args = minimist(process.argv.slice(2))
 if (args.number == null) 
     args.number = 1;
 let record = coinFlips(args.number);
+console.log(record);
 console.log(countFlips(record));
