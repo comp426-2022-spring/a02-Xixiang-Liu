1360ad0bea77fcf428ae6adf24f34911f5c82ac4
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 20:51:02 -0500

    again

--- flips.js
+++ flips.js
@@ -1,17 +1,20 @@
+import minimist from "minimist"
 import { coinFlips, countFlips } from "./modules/coin.mjs";
-import minimist from "minimist";
 
-const args = minimist(process.argv.slice(2));
-if (args.number == null)
-    args.number = 1;
-let record = coinFlips(args.number);
+const args = minimist(process.argv.slice(2))
+if (args.number == null) { args.number = 1 }
+
+let record = coinFlips(args.number)
+console.log(record)
+
 if (args.number <= 1) {
-    let data = countFlips(record);
+    let data = countFlips(record)
     if (data.heads == 1) {
-        console.log("{heads: " + data.heads + "}");
+        console.log("{heads: " + data.heads + "}")
     } else {
-        console.log("{tails: " + data.tails + "}");
+        console.log("{tails: " + data.tails + "}")
     }
 } else {
-    console.log(countFlips(record));
+    console.log(record)
+    console.log(countFlips(record))
 }
