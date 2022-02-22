699779cdd88f056646c3bbb0bb8c5608aedeca73
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 21:40:58 -0500

    again

--- guess-flip.js
+++ guess-flip.js
@@ -1,18 +1,21 @@
 import { flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 
-const args = minimist(process.argv.slice(2));
-if (args.number == null)
-    args.number = 1;
-let record = coinFlips(args.number);
-if (args.number <= 1) {
-    let data = countFlips(record);
-    if (data.heads == 1) {
-        console.log("{heads: " + data.heads + "}");
-    } else {
-        console.log("{tails: " + data.tails + "}");
+try {
+    const argv = process.argv.slice(2)[0].slice(7);
+    if (argv == 'heads' || argv == 'tails')
+        console.log(flipACoin(argv)); 
+    else
+        throw new Error;
+} 
+catch (error) {
+    console.log('Error: no input\nUsage: node guess-flip --call=[heads|tails]')
+    const args = minimist(process.argv.slice(2));
+    if (args.call == null || (args.call != "heads" && args.call != "tails")) {
+        console.log("Error: no input.");
+        console.log("Usage: node guess-flip --call=[heads|tails]");
     }
-} else {
-    console.log(record);
-    console.log(countFlips(record));
+    else {
+        console.log(flipACoin(args.call));
+    }
 }
