8506f8fd661d23366b0c355c1212cc7111645077
Xixiang-Liu <xxl@ad.unc.edu>
2022-02-21 21:30:56 -0500

    modify flips.js. Now if only one flip, one heads number or tails number will show up

--- guess-flip.js
+++ guess-flip.js
@@ -2,9 +2,17 @@ import { flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-if (args.call == null || (args.call != "heads" && args.call != "tails")) {
-    console.log("Error: no input.");
-    console.log("Usage: node guess-flip --call=[heads|tails]");
+if (args.number == null)
+    args.number = 1;
+let record = coinFlips(args.number);
+if (args.number <= 1) {
+    let data = countFlips(record);
+    if (data.heads == 1) {
+        console.log("{heads: " + data.heads + "}");
+    } else {
+        console.log("{tails: " + data.tails + "}");
+    }
 } else {
-    console.log(flipACoin(args.call));
+    console.log(record);
+    console.log(countFlips(record));
 }
